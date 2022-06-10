a77dc1350a9b646198eaeba837c628fb13e4c66d
tmoino <tony.moino@student.moringaschool.com>
2022-06-10 19:18:39 +0300

    assignent completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(src, sort) {
+   return src.filter
+     ((lettermatch) => lettermatch.toLowerCase() === sort.toLowerCase());
+ }
+ 
+
+ 
+ function fuzzyMatch(src, names ) {
+   return src.filter
+   ((lettermatch) => lettermatch.toLowerCase().indexOf(names.toLowerCase()) === 0);
+ }
+ 
+
+
+ function matchName(src, sortName) {
+   return src.filter((prnt) => prnt.name === sortName);
+ }
